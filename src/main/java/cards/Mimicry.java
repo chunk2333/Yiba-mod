43547b9f1fbbcff95e4cd7fe9b1c70422aff370b
yiba <chunk2333@gmail.com>
2023-02-26 20:01:34 +0800

    调整拟态：2 -> 1层无实体;费用3 -> 2;强化:消耗 -> 不消耗

--- src/main/java/cards/Mimicry.java
+++ src/main/java/cards/Mimicry.java
@@ -14,14 +14,14 @@ public class Mimicry extends CustomCard{
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String IMG_PATH = "img/cards_Seles/Mimicry.png";
-    private static final int COST = 3;
+    private static final int COST = 2;
     public static final String ID = "Mimicry";
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     //调用父类的构造方法，传参为super(卡牌ID,卡牌名称，能量花费，卡牌描述，卡牌类型，卡牌颜色，卡牌稀有度，卡牌目标)
     public Mimicry() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.SKILL, CardColor.GREEN, CardRarity.RARE, CardTarget.SELF);
         //设置 magicNumber
-        this.baseMagicNumber = 2;
+        this.baseMagicNumber = 1;
         this.magicNumber = this.baseMagicNumber;
         this.exhaust = true;
     }
@@ -42,7 +42,7 @@ public class Mimicry extends CustomCard{
         //卡牌升级后的效果
         if (!this.upgraded) {
             //更改费用
-            upgradeBaseCost(2);
+            //upgradeBaseCost(2);
             //更改名字和取消消耗
             upgradeName();
             this.exhaust = false;
