fcb17fa11527ca93222cb8419f103f86f6f120a0
yiba <chunk2333@gmail.com>
2023-03-03 15:51:53 +0800

    重写遗物：kakaa头上的镰刀

--- src/main/java/relics/kakaa.java
+++ src/main/java/relics/kakaa.java
@@ -1,14 +1,25 @@
 package relics;
 import basemod.abstracts.CustomRelic;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.TalkAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.InstantKillAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.combat.MindblastEffect;
+import com.megacrit.cardcrawl.vfx.combat.WeightyImpactEffect;
 
 
 public class kakaa extends CustomRelic {
@@ -18,7 +29,7 @@ public class kakaa extends CustomRelic {
     private boolean isActive = false;
     //调用父类的构造方法，传参为super(遗物ID,遗物全图，遗物白底图，遗物稀有度，获得遗物时的音效)
     public kakaa() {
-        super(ID, ImageMaster.loadImage(IMG), ImageMaster.loadImage(IMG_OTL), RelicTier.COMMON, AbstractRelic.LandingSound.CLINK);
+        super(ID, ImageMaster.loadImage(IMG), ImageMaster.loadImage(IMG_OTL), RelicTier.RARE, AbstractRelic.LandingSound.CLINK);
     }
 
     @Override
@@ -32,7 +43,33 @@ public class kakaa extends CustomRelic {
         //在用户使用牌时触发
 
     }
-
+    @Override
+    public void onBloodied() {
+        AbstractPlayer p = AbstractDungeon.player;
+        if(p.currentHealth==1){
+            flash();
+            addToTop(new RelicAboveCreatureAction(p, this));
+            //说话
+            addToBot(new TalkAction(true, "接受 #r@我的怒火吧~~~~~@ ", 1.5F, 2.0F));
+            //秒杀场上所有怪物
+            killAllMonsters(false);
+            //回满血
+            p.heal(p.maxHealth);
+        }
+    }
+    public void killAllMonsters(boolean isKillAllMode) {
+        if (AbstractDungeon.getMonsters() == null)
+            return;
+        for (AbstractMonster monster : (AbstractDungeon.getMonsters()).monsters) {
+            if (monster != null) {
+                AbstractDungeon.actionManager.addToTop(new InstantKillAction(monster));
+                if (!isKillAllMode) {
+                    AbstractDungeon.actionManager.addToTop(new WaitAction(0.8F));
+                    AbstractDungeon.actionManager.addToTop(new VFXAction(new WeightyImpactEffect(monster.hb.cX, monster.hb.cY)));
+                }
+            }
+        }
+    }
     @Override
     public void onVictory() {
         //在胜利时触发
@@ -46,7 +83,6 @@ public class kakaa extends CustomRelic {
     @Override
     //拾取时触发
     public void onEquip() {
-        AbstractDungeon.player.increaseMaxHp(114514, true);
     }
     @Override
     public AbstractRelic makeCopy() {
