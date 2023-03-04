d6491ebcf8f90b18396aabebcbb270a7d0acc1a4
yiba <chunk2333@gmail.com>
2023-03-04 16:55:47 +0800

    新增遗物：狗狗

---
+++ src/main/java/relics/Dog.java
@@ -0,0 +1,70 @@
+package relics;
+//狗狗
+
+import basemod.abstracts.CustomRelic;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+
+public class Dog extends CustomRelic {
+    public static final String ID = "Dog";
+    private static final String IMG = "img/relics_Seles/Dog.png";
+    private static final String IMG_OTL = "img/relics_Seles/outline/Dog.png";
+
+    //调用父类的构造方法，传参为super(遗物ID,遗物全图，遗物白底图，遗物稀有度，获得遗物时的音效)
+    public Dog() {
+        super(ID, ImageMaster.loadImage(IMG), ImageMaster.loadImage(IMG_OTL), RelicTier.UNCOMMON, LandingSound.SOLID);
+    }
+
+    @Override
+    public void atBattleStart() {
+        //在战斗开始时触发
+
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        //在用户使用牌时触发
+    }
+
+    @Override
+    public void onPlayerEndTurn() {
+        //玩家回合结束时触发
+        flash();
+        AbstractPlayer p = AbstractDungeon.player;
+        //遗物头顶特效
+        addToTop(new RelicAboveCreatureAction(p, this));
+        //回复1血
+        p.heal(1);
+        //获得1点力量
+        addToTop(new ApplyPowerAction(p, p, new StrengthPower(p, 1), 1));
+
+    }
+
+    @Override
+    public void onVictory() {
+        //在胜利时触发
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0];
+    }
+
+    @Override
+    //拾取时触发
+    public void onEquip() {
+    }
+
+    @Override
+    public AbstractRelic makeCopy() {
+        return new Dog();
+    }
+}
