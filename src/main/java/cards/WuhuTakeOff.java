88991e75ac573e0dc4090c9f801344a4396496f0
yiba <chunk2333@gmail.com>
2023-03-05 14:57:22 +0800

    新增：时光倒流，芜湖～起飞！，尖塔学 卡图

--- src/main/java/cards/WuhuTakeOff.java
+++ src/main/java/cards/WuhuTakeOff.java
@@ -14,7 +14,7 @@ public class WuhuTakeOff extends CustomCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("WuhuTakeOff");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "img/cards_Seles/test.png";
+    public static final String IMG_PATH = "img/cards_Seles/WuhuTakeOff.png";
     private static final int COST = 0;
     public static final String ID = "WuhuTakeOff";
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
