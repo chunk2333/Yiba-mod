3badeb2b9306078816e593a81431762b814116e5
yiba <chunk2333@gmail.com>
2023-03-07 18:04:19 +0800

    优化遗物：优化了TalkAction的触发方式

--- src/main/java/relics/HomoHead.java
+++ src/main/java/relics/HomoHead.java
@@ -2,20 +2,18 @@ package relics;
 
 import basemod.abstracts.CustomRelic;
 import basemod.patches.com.megacrit.cardcrawl.screens.stats.StatsScreen.UpdateStats;
+import cards.curse.DutifulSon;
+import cards.curse.YouAreOne_OneByOne;
+import cards.curse.desire;
+import cards.curse.snowman;
 import com.megacrit.cardcrawl.actions.animations.TalkAction;
-import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
-
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import cards.*;
 import pathes.SoundPlay;
 
 public class HomoHead extends CustomRelic {
@@ -37,7 +35,7 @@ public class HomoHead extends CustomRelic {
         //在战斗开始时触发
         AbstractCard card;
         int random;
-        random = AbstractDungeon.relicRng.random(1,3); //随机数
+        random = AbstractDungeon.relicRng.random(1,4); //随机数
         UpdateStats.logger.info("homo头随机数：" + random);
         switch (random){
             case 1 :
@@ -49,24 +47,24 @@ public class HomoHead extends CustomRelic {
             case 3:
                 card = new DutifulSon();
                 break;
+            case 4:
+                card = new YouAreOne_OneByOne();
+                break;
             default :
                 card = new snowman();
         }
         flash();
         //遗物特效，在头顶显示
         addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
-
-        //CardCrawlGame.sound.
-        addToBot(new TalkAction(true, "哼！哼！哼！啊啊啊啊啊啊啊啊啊啊啊啊！！！！", 1.0F, 2.0F));
-        //s.playV("HomoVoice",10);
-        //s.adjustVolume("HomoVoice",10,10);
         int random_play;
         random_play = AbstractDungeon.relicRng.random(1,100); //随机数
         if(random_play<=5){
             s.play("HomoVoice",true);
+            addToBot(new TalkAction(true, "哼！哼！哼！啊啊啊啊啊啊啊啊啊啊啊啊！！！！", 1.0F, 2.0F));
         }
         if(random_play>5 && random_play<=10){
             s.play("HomoVoice",true);
+            addToBot(new TalkAction(true, "哼！哼！哼！啊啊啊啊啊啊啊啊啊啊啊啊！！！！", 1.0F, 2.0F));
         }
         //往抽牌堆里塞一张“食雪汉”诅咒
         addToBot(new MakeTempCardInDrawPileAction(card, 1, true, true));
@@ -81,15 +79,10 @@ public class HomoHead extends CustomRelic {
     @Override
     public void atTurnStart() {
         //回合开始时触发
-        //flash();
-        //AbstractPlayer p = AbstractDungeon.player;
-        //loseHP
-        //addToBot(new LoseHPAction(p, p, 1));
     }
     @Override
     public void onVictory() {
         //在胜利时触发
-
     }
 
     @Override
