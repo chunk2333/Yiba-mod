a3c1cd14e0d1c8453e8c93dc49a75fbb570d35d1
yiba <chunk2333@gmail.com>
2023-03-10 14:53:42 +0800

    削弱遗物：海豹 普通 -> 稀有

--- src/main/java/relics/Seals.java
+++ src/main/java/relics/Seals.java
@@ -1,35 +1,20 @@
-//Seals
 package relics;
+//海豹
+
 import basemod.abstracts.CustomRelic;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.powers.StrengthPower;
-
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.BetterDrawPileToHandAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.localization.CardStrings;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 
 public class Seals extends CustomRelic {
     public static final String ID = "Seals";
     private static final String IMG = "img/relics_Seles/Seals.png";
     private static final String IMG_OTL = "img/relics_Seles/outline/Seals.png";
 
-    //调用父类的构造方法，传参为super(遗物ID,遗物全图，遗物白底图，遗物稀有度，获得遗物时的音效)
     public Seals() {
-        super(ID, ImageMaster.loadImage(IMG), ImageMaster.loadImage(IMG_OTL), RelicTier.COMMON, LandingSound.MAGICAL);
+        super(ID, ImageMaster.loadImage(IMG), ImageMaster.loadImage(IMG_OTL), RelicTier.RARE, LandingSound.MAGICAL);
     }
 
     @Override
@@ -37,6 +22,7 @@ public class Seals extends CustomRelic {
         //战斗开始时，抽卡时触发
         addToBot(new BetterDrawPileToHandAction(1));
     }
+
     @Override
     public void atBattleStart() {
         //在战斗开始时触发
@@ -56,11 +42,13 @@ public class Seals extends CustomRelic {
     public String getUpdatedDescription() {
         return this.DESCRIPTIONS[0];
     }
+
     @Override
     //拾取时触发
     public void onEquip() {
 
     }
+
     @Override
     public AbstractRelic makeCopy() {
         return new Seals();
