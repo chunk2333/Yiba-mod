b4b8f72c9999d5834cbaa815bffa740c5a58eab6
yiba <chunk2333@gmail.com>
2023-03-13 15:47:48 +0800

    更新卡牌：肉身震撼 卡图

--- src/main/java/cards/red/FleshShock.java
+++ src/main/java/cards/red/FleshShock.java
@@ -19,7 +19,7 @@ public class FleshShock extends CustomCard{
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("FleshShock");
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "img/cards_Seles/test.png";
+    public static final String IMG_PATH = "img/cards_Seles/FleshShock.png";
     private static final int COST = 3;
     public static final String ID = "FleshShock";
     public FleshShock() {
