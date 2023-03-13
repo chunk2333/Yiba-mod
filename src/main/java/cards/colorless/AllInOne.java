c624df754a937644e5032b915662d92faa8c5f81
yiba <chunk2333@gmail.com>
2023-03-13 15:48:05 +0800

    新增卡牌：知行合一

---
+++ src/main/java/cards/colorless/AllInOne.java
@@ -0,0 +1,64 @@
+package cards.colorless;
+//知行合一
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class AllInOne extends CustomCard{
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("AllInOne");
+
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String IMG_PATH = "img/cards_Seles/AllInOne.png";
+
+    private static final int COST = 0;
+
+    public static final String ID = "AllInOne";
+
+    public AllInOne() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.ATTACK, CardColor.COLORLESS, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        //添加基础攻击标签和将伤害设为6
+        this.baseDamage = 5;
+        this.damage = this.baseDamage;
+        this.baseMagicNumber = 3;
+        this.magicNumber = this.baseMagicNumber;
+        //消耗
+        this.exhaust = true;
+
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //使用卡牌时触发的动作
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        p.heal(this.magicNumber);
+
+    }
+
+    @Override
+
+    public AbstractCard makeCopy() {
+        //复制卡牌时触发
+        return new AllInOne();
+    }
+
+    @Override
+    public void upgrade() {
+        //卡牌升级后的效果
+        if (!this.upgraded) {
+            //更改名字和提高3点伤害
+            upgradeName();
+            upgradeDamage(3);
+            upgradeMagicNumber(2);
+        }
+    }
+}
