02104a5907b8dc4dcb0b2dd38fcf06a12231a2fa
chunk2333 <chunk2333@gmail.com>
2023-03-15 19:58:31 +0800

    新增卡牌：强健体魄

--- src/main/java/YibaMod/YibaMod.java
+++ src/main/java/YibaMod/YibaMod.java
@@ -239,6 +239,7 @@ public class YibaMod implements RelicGetSubscriber, PostPowerApplySubscriber, Po
         this.cardsToAdd.add(new VR());//虚拟现实
         this.cardsToAdd.add(new ChoseMiracle());//天若有情
         this.cardsToAdd.add(new ReturnGoodForEvil());//以德报怨
+        this.cardsToAdd.add(new StrongPhysique());//强健体魄
     }
 
     @Override
---
+++ src/main/java/cards/colorless/StrongPhysique.java
@@ -0,0 +1,60 @@
+package cards.colorless;
+//强健体魄
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+public class StrongPhysique extends CustomCard {
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("StrongPhysique");
+
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String IMG_PATH = "img/cards/StrongPhysique.png";
+
+    private static final int COST = 1;
+
+    public static final String ID = "StrongPhysique";
+
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    public StrongPhysique() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.POWER, CardColor.COLORLESS, CardRarity.UNCOMMON, CardTarget.SELF);
+        this.baseMagicNumber = 2;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //使用卡牌时触发的动作
+
+        //力量
+        addToTop(new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber));
+        //敏捷
+        addToTop(new ApplyPowerAction(p, p, new DexterityPower(p, this.magicNumber), this.magicNumber));
+
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        //复制卡牌时触发
+        return new StrongPhysique();
+    }
+
+    @Override
+    public void upgrade() {
+        //卡牌升级后的效果
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+        }
+    }
+
+}
