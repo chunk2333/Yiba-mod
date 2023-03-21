85944ae3b441c8de13239fcd50c542ef43b7ab4b
chunk2333 <chunk2333@gmail.com>
2023-03-21 21:43:26 +0800

    更新能力：元素精通

--- src/main/java/power/MysteryPower.java
+++ src/main/java/power/MysteryPower.java
@@ -2,12 +2,15 @@ package power;
 //元素精通
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.unlock.UnlockTracker;
 
 public class MysteryPower extends AbstractPower {
     public static final String POWER_ID = "MysteryPower";
@@ -23,19 +26,63 @@ public class MysteryPower extends AbstractPower {
         super();
         atlas_self = new TextureAtlas(Gdx.files.internal("powers/Selfpowers.atlas"));
         this.name = NAME;
-        updateDescription();
         this.ID = POWER_ID ;
         this.owner = owner;
         this.amount = amt;
+        if (this.amount >= 999) {
+            this.amount = 999;
+        }
+        if (this.amount <= -999) {
+            this.amount = -999;
+        }
         updateDescription();
         this.region48 = atlas_self.findRegion("48/DoubleDamage");
         this.region128 = atlas_self.findRegion("128/DoubleDamage");
-        this.type = AbstractPower.PowerType.BUFF;
+        this.canGoNegative = true;
     }
 
     public void atStartOfTurn(){
         //回合开始时
     }
+
+    public void stackPower(int stackAmount) {
+        this.fontScale = 8.0F;
+        this.amount += stackAmount;
+        if (this.amount == 0) {
+            this.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, this.ID));
+        }
+
+        if (this.amount >= 50 && this.owner == AbstractDungeon.player) {
+            //UnlockTracker.unlockAchievement("JAXXED");
+        }
+
+        if (this.amount >= 999) {
+            this.amount = 999;
+        }
+
+        if (this.amount <= -999) {
+            this.amount = -999;
+        }
+
+    }
+
+    public void reducePower(int reduceAmount) {
+        this.fontScale = 8.0F;
+        this.amount -= reduceAmount;
+        if (this.amount == 0) {
+            this.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, this.ID));
+        }
+
+        if (this.amount >= 999) {
+            this.amount = 999;
+        }
+
+        if (this.amount <= -999) {
+            this.amount = -999;
+        }
+
+    }
+
     public void updateDescription() {
         this.description = powerStrings.DESCRIPTIONS[0] + this.amount + powerStrings.DESCRIPTIONS[1];
     }
