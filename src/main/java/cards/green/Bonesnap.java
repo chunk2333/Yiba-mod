061537271c8bd2ee18d49cf120a70882d29dce9e
chunk2333 <chunk2333@gmail.com>
2023-03-21 08:52:20 +0800

    削弱卡牌：碎骨 8/11 -> 8 即升级不再加伤害，只加临时力量

--- src/main/java/cards/green/Bonesnap.java
+++ src/main/java/cards/green/Bonesnap.java
@@ -13,42 +13,39 @@ import com.megacrit.cardcrawl.powers.*;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 
 public class Bonesnap extends CustomCard{
+
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("Bonesnap");
+
     public static final String NAME = cardStrings.NAME;
+
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
     public static final String IMG_PATH = "img/cards/Bonesnap.png";
+
     private static final int COST = 1;
+
     public static final String ID = "Bonesnap";
-    //public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    //调用父类的构造方法，传参为super(卡牌ID,卡牌名称，能量花费，卡牌描述，卡牌类型，卡牌颜色，卡牌稀有度，卡牌目标)
+
     public Bonesnap() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.ATTACK, CardColor.GREEN, CardRarity.COMMON, CardTarget.ENEMY);
-        //添加基础攻击标签和将伤害设为6
         this.baseDamage = 8;
-
         this.damage = this.baseDamage;
-
         this.baseMagicNumber=2;
-
         this.magicNumber=this.baseMagicNumber;
-
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         //使用卡牌时触发的动作
-
         //给予伤害
         addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
-        //获得3点临时力量
+        //获得临时力量
         addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, this.magicNumber), this.magicNumber));
         addToBot(new ApplyPowerAction(p, p, new LoseStrengthPower(p, this.magicNumber), this.magicNumber));
-
-
     }
+
     @Override
     public AbstractCard makeCopy() {
-        //复制卡牌时触发
         return new Bonesnap();
     }
 
@@ -58,7 +55,7 @@ public class Bonesnap extends CustomCard{
         if (!this.upgraded) {
             //更改名字和提高3点伤害
             upgradeName();
-            upgradeDamage(3);
+            //upgradeDamage(3);
             upgradeMagicNumber(2);
         }
     }
