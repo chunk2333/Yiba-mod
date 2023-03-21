3c51a0a18fc11e9c1a8d6cff0a07f5d3ef0d589a
chunk2333 <chunk2333@gmail.com>
2023-03-21 20:04:04 +0800

    新增卡牌：水波术

---
+++ src/main/java/cards/element/WaterWaveTechnique.java
@@ -0,0 +1,63 @@
+package cards.element;
+//水波术
+import Tools.YiBaHelper;
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import pathes.AbstractCardEnum;
+import power.HydroPower;
+
+public class WaterWaveTechnique extends CustomCard {
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("WaterWaveTechnique");
+
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String IMG_PATH = "img/cards/witch/WaterWaveTechnique.png";
+
+    private static final int COST = 1;
+
+    public static final String ID = "WaterWaveTechnique";
+
+    public WaterWaveTechnique() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.ATTACK, AbstractCardEnum.Witch_COLOR, CardRarity.BASIC, CardTarget.SELF_AND_ENEMY);
+        this.baseDamage = 4;
+        this.damage = this.baseDamage;
+        this.baseBlock = 4;
+        this.block = this.baseBlock;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //使用卡牌时触发的动作
+        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
+        addToBot(new ApplyPowerAction(m, m, new HydroPower(m, YiBaHelper.getPlayerMystery()),1));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        //复制卡牌时触发
+        return new WaterWaveTechnique();
+    }
+
+    @Override
+    public void upgrade() {
+        //卡牌升级后的效果
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+    }
+}
