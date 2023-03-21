13b216094b70e1d56b92388fe4a6fa0f308c1869
chunk2333 <chunk2333@gmail.com>
2023-03-21 20:04:24 +0800

    新增方法：取元素精通

--- src/main/java/Tools/YiBaHelper.java
+++ src/main/java/Tools/YiBaHelper.java
@@ -1,12 +1,27 @@
 package Tools;
 
-
 import com.evacipated.cardcrawl.modthespire.Loader;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
 public class YiBaHelper {
     public static String MakeSoundPath(String id){
         return "YiBa_Sound:" + id;
     }
+
     public static Boolean hasMod(String modId){
         return Loader.isModLoadedOrSideloaded(modId);
+    }
+
+    public static int getPlayerMystery(){
+        int mystery = 0;
+        AbstractPlayer p = AbstractDungeon.player;
+        for(AbstractPower power:p.powers){
+            if(power.ID.equals("MysteryPower")){
+                mystery = power.amount;
+            }
+        }
+        return mystery;
     }
 }
