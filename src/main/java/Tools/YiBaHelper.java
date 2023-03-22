653fa5f086ec08bb8d0140636d3992a3798c2d87
chunk2333 <chunk2333@gmail.com>
2023-03-22 20:21:32 +0800

    新增卡牌：魔力压榨

--- src/main/java/Tools/YiBaHelper.java
+++ src/main/java/Tools/YiBaHelper.java
@@ -1,9 +1,13 @@
 package Tools;
 
+import YibaMod.YibaMod;
 import com.evacipated.cardcrawl.modthespire.Loader;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import java.util.ArrayList;
 
 public class YiBaHelper {
     public static String MakeSoundPath(String id){
@@ -23,5 +27,16 @@ public class YiBaHelper {
             }
         }
         return mystery;
+    }
+
+    public static AbstractCard getRandomCurseCard(){
+        //取随机诅咒牌
+        ArrayList<AbstractCard> list = new ArrayList<>();
+        for (AbstractCard c : AbstractDungeon.curseCardPool.group) {
+            if(c.type== AbstractCard.CardType.CURSE){
+                list.add(c);
+            }
+        }
+        return list.get(AbstractDungeon.cardRandomRng.random(list.size() - 1));
     }
 }
