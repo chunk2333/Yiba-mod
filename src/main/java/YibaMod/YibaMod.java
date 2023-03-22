23f056f17b06a0789b6091f8e974d7413b32f21b
chunk2333 <chunk2333@gmail.com>
2023-03-22 10:08:37 +0800

    新增卡牌：死神降临

--- src/main/java/YibaMod/YibaMod.java
+++ src/main/java/YibaMod/YibaMod.java
@@ -185,6 +185,8 @@ public class YibaMod implements RelicGetSubscriber, PostPowerApplySubscriber, Po
         this.cardsToAdd.add(new Clumsy_My());//弄巧成拙
         this.cardsToAdd.add(new WaterMirror());//水镜
         this.cardsToAdd.add(new NairaDescends());//奈落降临
+        this.cardsToAdd.add(new LookingGlass());//镜花水月
+        this.cardsToAdd.add(new DeathComes());//死神降临
         //this.cardsToAdd.add(new SavePower());
         //this.cardsToAdd.add(new Printf());
 
