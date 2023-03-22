47fad24a601b2999e5ba68c772d3ad7fa5999bee
chunk2333 <chunk2333@gmail.com>
2023-03-22 10:37:51 +0800

    新增卡牌：重修

--- src/main/java/YibaMod/YibaMod.java
+++ src/main/java/YibaMod/YibaMod.java
@@ -188,6 +188,7 @@ public class YibaMod implements RelicGetSubscriber, PostPowerApplySubscriber, Po
         this.cardsToAdd.add(new LookingGlass());//镜花水月
         this.cardsToAdd.add(new DeathComes());//死神降临
         this.cardsToAdd.add(new InflammationOfTheWheel());//转轮之炎
+        this.cardsToAdd.add(new Relearn());//重修
         //this.cardsToAdd.add(new SavePower());
         //this.cardsToAdd.add(new Printf());
 
