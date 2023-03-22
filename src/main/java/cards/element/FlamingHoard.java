5b42f1f3170c059d4c1a53fd622543809e72e876
chunk2333 <chunk2333@gmail.com>
2023-03-22 13:28:15 +0800

    新增卡牌：烈焰囤积

---
+++ src/main/java/cards/element/FlamingHoard.java
@@ -0,0 +1,68 @@
+package cards.element;
+//烈焰囤积
+import Tools.YiBaHelper;
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ModifyDamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import pathes.AbstractCardEnum;
+import power.PyroPower;
+
+public class FlamingHoard extends CustomCard {
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("FlamingHoard");
+
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String IMG_PATH = "img/cards/witch/FlamingHoard.png";
+
+    private static final int COST = 1;
+
+    public static final String ID = "FlamingHoard";
+
+    public FlamingHoard() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.ATTACK, AbstractCardEnum.Witch_COLOR, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        this.baseDamage = 3;
+        this.damage = this.baseDamage;
+        this.baseMagicNumber = 3;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //使用卡牌时触发的动作
+        //给予火元素
+        addToBot(new ApplyPowerAction(m, m, new PyroPower(m, YiBaHelper.getPlayerMystery()), 1));
+        //造成伤害
+        for (int i = 0; i < this.magicNumber; i++){
+            addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        }
+        //基础伤害+1
+        addToBot(new ModifyDamageAction(this.uuid, 1));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        //复制卡牌时触发
+        return new FlamingHoard();
+    }
+
+    @Override
+    public void upgrade() {
+        //卡牌升级后的效果
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+            initializeDescription();
+        }
+    }
+}
