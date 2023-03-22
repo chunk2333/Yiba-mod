1d1b8e928beeeb1e9313712622e6ed0acec3e3f6
chunk2333 <chunk2333@gmail.com>
2023-03-22 13:28:48 +0800

    add

---
+++ src/main/java/cards/element/FlamingStorm.java
@@ -0,0 +1,72 @@
+package cards.element;
+//烈焰风暴
+import Tools.YiBaHelper;
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.LightningEffect;
+import pathes.AbstractCardEnum;
+import power.PyroPower;
+
+public class FlamingStorm extends CustomCard {
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("FlamingStorm");
+
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String IMG_PATH = "img/cards/witch/FlamingStorm.png";
+
+    private static final int COST = 1;
+
+    public static final String ID = "FlamingStorm";
+
+    public FlamingStorm() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.ATTACK, AbstractCardEnum.Witch_COLOR, CardRarity.COMMON, CardTarget.ALL_ENEMY);
+        this.isMultiDamage = true;
+        this.baseDamage = 5;
+        this.damage = this.baseDamage;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //使用卡牌时触发的动作
+        //造成伤害
+        addToBot(new SFXAction("THUNDERCLAP", 0.05F));
+        for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters) {
+            if (!mo.isDeadOrEscaped())
+                addToBot(new VFXAction(new LightningEffect(mo.drawX, mo.drawY), 0.05F));
+        }
+
+        addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
+        for (AbstractMonster mo : (AbstractDungeon.getCurrRoom()).monsters.monsters){
+            //给予火元素
+            addToBot(new ApplyPowerAction(mo, mo, new PyroPower(mo, YiBaHelper.getPlayerMystery()), 1));
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        //复制卡牌时触发
+        return new FlamingStorm();
+    }
+
+    @Override
+    public void upgrade() {
+        //卡牌升级后的效果
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeDamage(3);
+        }
+    }
+}
