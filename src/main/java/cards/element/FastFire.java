3626ba0b06a02100b66e50b47aee594c8e4234c5
chunk2333 <chunk2333@gmail.com>
2023-03-22 11:13:48 +0800

    新增卡牌：快速火焰

---
+++ src/main/java/cards/element/FastFire.java
@@ -0,0 +1,55 @@
+package cards.element;
+//快速火焰
+import Tools.YiBaHelper;
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import pathes.AbstractCardEnum;
+import power.PyroPower;
+
+
+public class FastFire extends CustomCard {
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("FastFire");
+
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String IMG_PATH = "img/cards/witch/FastFire.png";
+
+    private static final int COST = 1;
+
+    public static final String ID = "FastFire";
+
+    public FastFire() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.SKILL, AbstractCardEnum.Witch_COLOR, CardRarity.COMMON, CardTarget.ENEMY);
+        this.exhaust = true;
+        this.selfRetain = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //使用卡牌时触发的动作
+        //给予 火元素
+        addToBot(new ApplyPowerAction(m, m, new PyroPower(m, YiBaHelper.getPlayerMystery()), 1));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        //复制卡牌时触发
+        return new FastFire();
+    }
+
+    @Override
+    public void upgrade() {
+        //卡牌升级后的效果
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+    }
+}
