83bc016ce2b4e928fa4b4e4f2fdb11783ff703db
chunk2333 <chunk2333@gmail.com>
2023-03-23 08:40:29 +0800

    更新卡牌：爆裂魔法 卡图

--- src/main/java/cards/element/Explosion.java
+++ src/main/java/cards/element/Explosion.java
@@ -28,7 +28,7 @@ public class Explosion extends CustomCard {
 
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
-    public static final String IMG_PATH = "img/cards/witch/test.png";
+    public static final String IMG_PATH = "img/cards/witch/Explosion.png";
 
     private static final int COST = 3;
 
