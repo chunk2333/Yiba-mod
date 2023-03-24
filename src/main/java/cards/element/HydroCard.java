7bc469958dff4f3a6228a70dd298e8c7eef950e4
chunk2333 <chunk2333@gmail.com>
2023-03-24 20:29:04 +0800

    add

--- src/main/java/cards/element/HydroCard.java
+++ src/main/java/cards/element/HydroCard.java
@@ -13,16 +13,21 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 import pathes.AbstractCardEnum;
 import power.HydroPower;
 
 public class HydroCard extends CustomCard{
+
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("HydroCard");
+
     public static final String NAME = cardStrings.NAME;
+
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
     public static final String IMG_PATH = "img/cards/test.png";
+
     private static final int COST = 0;
+
     public static final String ID = "HydroCard";
     public HydroCard() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.ATTACK, AbstractCardEnum.Witch_COLOR, CardRarity.UNCOMMON, CardTarget.ENEMY);
