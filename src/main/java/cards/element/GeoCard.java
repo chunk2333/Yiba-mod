9c5862eb822244b68c7e443b3dab003e55f09636
chunk2333 <chunk2333@gmail.com>
2023-03-26 10:43:28 +0800

    调整卡牌：岩元素牌

--- src/main/java/cards/element/GeoCard.java
+++ src/main/java/cards/element/GeoCard.java
@@ -3,14 +3,10 @@ package cards.element;
 import Tools.YiBaHelper;
 import YibaMod.YibaMod;
 import basemod.abstracts.CustomCard;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import power.GeoPower;
@@ -20,7 +16,7 @@ public class GeoCard extends CustomCard{
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String IMG_PATH = "img/cards/witch/GeoCard.png";
-    private static final int COST = 2;
+    private static final int COST = 1;
     public static final String ID = "GeoCard";
     public GeoCard() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.SKILL, CardColor.COLORLESS, CardRarity.SPECIAL, CardTarget.ENEMY);
