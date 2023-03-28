783ce7c2eb249c82689d455738a09733235aebe2
chunk2333 <chunk2333@gmail.com>
2023-03-28 08:58:20 +0800

    新增卡牌：柔水盾(无图)

--- src/main/java/YibaMod/YibaMod.java
+++ src/main/java/YibaMod/YibaMod.java
@@ -240,6 +240,8 @@ public class YibaMod implements RelicGetSubscriber, PostPowerApplySubscriber, Po
         this.cardsToAdd.add(new TheSage());//贤者
         this.cardsToAdd.add(new SimpleMagic());//简易魔法
         this.cardsToAdd.add(new LiquidFire());//液态火
+        this.cardsToAdd.add(new PleaseKeepBack());//生人勿近
+        this.cardsToAdd.add(new SoftWaterShield());//柔水盾
         //this.cardsToAdd.add(new SavePower());
         //this.cardsToAdd.add(new Printf());
 
