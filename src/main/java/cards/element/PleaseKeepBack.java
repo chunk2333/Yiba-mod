274da191bdf02cae73a03a2b9933f209834987d5
chunk2333 <chunk2333@gmail.com>
2023-03-28 08:58:04 +0800

    新增卡牌：生人勿近(无图标)

---
+++ src/main/java/cards/element/PleaseKeepBack.java
@@ -0,0 +1,62 @@
+package cards.element;
+//生人勿近
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import pathes.AbstractCardEnum;
+import power.PleaseKeepBackPower;
+
+public class PleaseKeepBack extends CustomCard {
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("PleaseKeepBack");
+
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String IMG_PATH = "img/cards/witch/test.png";
+
+    private static final int COST = 1;
+
+    public static final String ID = "PleaseKeepBack";
+
+    public PleaseKeepBack() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.ATTACK, AbstractCardEnum.Witch_COLOR, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        this.baseMagicNumber = 2;
+        this.magicNumber = this.baseMagicNumber;
+        this.baseDamage = 3;
+        this.exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //使用卡牌时触发的动作
+        //造成伤害
+        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        //给予buff
+        addToBot(new ApplyPowerAction(m, p, new PleaseKeepBackPower(m, this.magicNumber),this.magicNumber));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        //复制卡牌时触发
+        return new PleaseKeepBack();
+    }
+
+    @Override
+    public void upgrade() {
+        //卡牌升级后的效果
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+            upgradeDamage(2);
+        }
+    }
+}
