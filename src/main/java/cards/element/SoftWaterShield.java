d4e3462eefe2a171fc601976b77c1be8f67f7e78
chunk2333 <chunk2333@gmail.com>
2023-03-29 19:22:18 +0800

    更新卡牌：柔水盾 卡图

--- src/main/java/cards/element/SoftWaterShield.java
+++ src/main/java/cards/element/SoftWaterShield.java
@@ -21,7 +21,7 @@ public class SoftWaterShield extends CustomCard {
 
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
-    public static final String IMG_PATH = "img/cards/witch/test.png";
+    public static final String IMG_PATH = "img/cards/witch/SoftWaterShield.png";
 
     private static final int COST = 1;
 
