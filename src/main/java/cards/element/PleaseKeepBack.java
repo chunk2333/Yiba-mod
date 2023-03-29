20962242ce73963da524aa35118512a47f675a92
chunk2333 <chunk2333@gmail.com>
2023-03-29 19:27:48 +0800

    更新卡牌：生人勿近 卡图

--- src/main/java/cards/element/PleaseKeepBack.java
+++ src/main/java/cards/element/PleaseKeepBack.java
@@ -21,7 +21,7 @@ public class PleaseKeepBack extends CustomCard {
 
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
 
-    public static final String IMG_PATH = "img/cards/witch/test.png";
+    public static final String IMG_PATH = "img/cards/witch/PleaseKeepBack.png";
 
     private static final int COST = 1;
 
