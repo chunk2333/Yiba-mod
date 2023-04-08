4d1fa8252dbab0d3a2249c609f174e84c6df094d
chunk2333 <chunk2333@gmail.com>
2023-04-08 18:32:58 +0800

    新增卡牌：狂飓切裂

--- src/main/java/YibaMod/YibaMod.java
+++ src/main/java/YibaMod/YibaMod.java
@@ -255,6 +255,7 @@ public class YibaMod implements RelicGetSubscriber, PostPowerApplySubscriber, Po
         this.cardsToAdd.add(new Annihilate());//湮灭
         this.cardsToAdd.add(new HyperElements());//高浓度元素
         this.cardsToAdd.add(new MagicOverflow());//魔力溢出
+        this.cardsToAdd.add(new CrazyHurricaneCut());//狂飓切裂
         //this.cardsToAdd.add(new SavePower());
         //this.cardsToAdd.add(new Printf());
 
