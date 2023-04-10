3456dab8354ce3d4dc54a3c150aca577e6bbc4e3
chunk2333 <chunk2333@gmail.com>
2023-04-10 09:19:46 +0800

    新增卡牌：瞬身切割

---
+++ src/main/java/cards/element/InstantCutting.java
@@ -0,0 +1,64 @@
+package cards.element;
+//瞬身切割
+import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.SweepingBeamEffect;
+import pathes.AbstractCardEnum;
+
+public class InstantCutting extends CustomCard {
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("InstantCutting");
+
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String ID = "InstantCutting";
+
+    private static final int COST = 1;
+
+    public static final String IMG_PATH = "img/cards/witch/test.png";
+
+    public InstantCutting() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.ATTACK, AbstractCardEnum.Witch_COLOR, CardRarity.COMMON, CardTarget.ALL_ENEMY);
+        this.baseDamage = 6;
+        this.isMultiDamage = true;
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        //使用卡牌时触发的动作
+        addToBot(new SFXAction("ATTACK_DEFECT_BEAM"));
+        addToBot(new VFXAction(p, new SweepingBeamEffect(AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY, AbstractDungeon.player.flipHorizontal), 0.4F));
+        addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
+        addToBot(new DrawCardAction(p, this.magicNumber));
+
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        //复制卡牌时触发
+        return new InstantCutting();
+    }
+
+    @Override
+    public void upgrade() {
+        //卡牌升级后的效果
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+        }
+    }
+}
