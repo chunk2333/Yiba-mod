192f23e3bbb728977da30774d152d00ba6bc158c
chunk2333 <chunk2333@gmail.com>
2023-04-22 08:24:10 +0800

    新增卡牌：火耕水耨

--- src/main/java/cards/colorless/Spiritual.java
+++ src/main/java/cards/colorless/Spiritual.java
@@ -8,7 +8,6 @@ import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
