ad57319f53a2a399d83da8ac6fbe67290ddcbdd7
chunk2333 <chunk2333@gmail.com>
2023-05-01 06:28:38 +0800

    更新遗物：纠缠之缘 不在48层后出现

--- src/main/java/relics/IntertwinedFate.java
+++ src/main/java/relics/IntertwinedFate.java
@@ -5,6 +5,7 @@ import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
@@ -42,6 +43,11 @@ public class IntertwinedFate extends CustomRelic {
     @Override
     public String getUpdatedDescription() {
         return this.DESCRIPTIONS[0];
+    }
+
+    @Override
+    public boolean canSpawn() {
+        return (Settings.isEndless || AbstractDungeon.floorNum <= 48);
     }
 
     @Override
