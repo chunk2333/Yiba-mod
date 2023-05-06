2354c799b9108808dfa05767626e10ed82ce3a3f
chunk2333 <chunk2333@gmail.com>
2023-05-06 20:00:40 +0800

    削弱卡牌：出千 添加虚无词条

--- src/main/java/cards/green/happyOldHome.java
+++ src/main/java/cards/green/happyOldHome.java
@@ -1,4 +1,5 @@
 package cards.green;
+//出千
 import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -9,44 +10,46 @@ import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import power.*;
 
 public class happyOldHome extends CustomCard{
-    //从.json文件中提取键名为LeiPu的信息
+
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("happyOldHome");
+
     public static final String NAME = cardStrings.NAME;
+
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
     public static final String IMG_PATH = "img/cards/happyOldHome.png";
+
     private static final int COST = 3;
+
     public static final String ID = "happyOldHome";
-    //public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    //调用父类的构造方法，传参为super(卡牌ID,卡牌名称，能量花费，卡牌描述，卡牌类型，卡牌颜色，卡牌稀有度，卡牌目标)
+
     public happyOldHome() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.POWER, CardColor.GREEN, CardRarity.RARE, CardTarget.SELF);
-        //设置 magicNumber
         this.baseMagicNumber = 1;
         this.magicNumber = this.baseMagicNumber;
-
+        this.isEthereal = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         //使用卡牌时触发的动作
         addToBot(new ApplyPowerAction(p, p, new happyOldHomePower(p, this.magicNumber), this.magicNumber));
-
     }
 
     @Override
-
     public AbstractCard makeCopy() {
         //复制卡牌时触发
         return new happyOldHome();
     }
+
     @Override
     public void upgrade() {
         //卡牌升级后的效果
         if (!this.upgraded) {
             //更改名字和费用
-
             upgradeName();
             upgradeBaseCost(2);
         }
     }
+
 }
