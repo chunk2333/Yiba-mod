bf3f6443ee34c630c48824b3578a966e67a48214
chunk2333 <chunk2333@gmail.com>
2023-06-02 11:08:01 +0800

    削弱卡牌：天地同寿 强化不在额外提升10点伤害

--- src/main/java/cards/purple/LongevityInHeavenAndEarth.java
+++ src/main/java/cards/purple/LongevityInHeavenAndEarth.java
@@ -12,15 +12,21 @@ import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class LongevityInHeavenAndEarth extends CustomCard {
+
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("LongevityInHeavenAndEarth");
+
     public static final String NAME = cardStrings.NAME;
+
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
     public static final String IMG_PATH = "img/cards/LongevityInHeavenAndEarth.png";
+
     private static final int COST = 4;
+
     public static final String ID = "LongevityInHeavenAndEarth";
+
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
-    //调用父类的构造方法，传参为super(卡牌ID,卡牌名称，能量花费，卡牌描述，卡牌类型，卡牌颜色，卡牌稀有度，卡牌目标)
     public LongevityInHeavenAndEarth() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.ATTACK, CardColor.PURPLE, CardRarity.RARE, CardTarget.ENEMY);
         this.baseDamage = 30;
@@ -29,8 +35,6 @@ public class LongevityInHeavenAndEarth extends CustomCard {
     }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        //使用卡牌时触发的动作
-
         //造成伤害
         addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HEAVY));
         //进入神格
@@ -47,7 +51,7 @@ public class LongevityInHeavenAndEarth extends CustomCard {
         if (!this.upgraded) {
             //更改名字和伤害
             upgradeName();
-            upgradeDamage(10);
+            //upgradeDamage(10);
             //设置保留
             this.selfRetain = true;
             //读取升级后的描述
