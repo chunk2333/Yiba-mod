f2b6a2c7d3f836d5985e6140d0d435393d100536
chunk2333 <chunk2333@gmail.com>
2023-10-05 20:07:07 +0800

    实现为原版SoundMaster添加自定义音频，使用CardCrawlGame.sound.playA播放

---
+++ src/main/java/pathes/SoundMasterPatch.java
@@ -0,0 +1,19 @@
+package pathes;
+
+
+import YibaMod.YibaMod;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.audio.Sfx;
+
+import java.util.HashMap;
+
+@SpirePatch(cls = "com.megacrit.cardcrawl.audio.SoundMaster", method = SpirePatch.CONSTRUCTOR)
+public class SoundMasterPatch {
+    @SpireInsertPatch(loc = 410, localvars={"map"})
+    public static void addVoice(HashMap<String, Sfx> ___map) {
+        YibaMod.logger.info("开始加载自定义声音...");
+        ___map.put("HelloLBW", new Sfx("sound/HelloLBW.mp3", false));
+        ___map.put("Fall_Steel", new Sfx("sound/Steelpipefalling.mp3", false));
+    }
+}
