f0c405a5bb05d222b7f708106362ed6a301ff715
chunk2333 <chunk2333@gmail.com>
2023-10-27 13:38:09 +0800

    新增卡牌：虚拟机(蓝色)。为卡牌：指鹿为马 添加测试画风卡牌封面。

---
+++ src/main/java/cards/blue/Virtual.java
@@ -0,0 +1,51 @@
+package cards.blue;
+//虚拟机
+import basemod.abstracts.CustomCard;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class Virtual extends CustomCard {
+
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("Virtual");
+
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String IMG_PATH = "img/cards/Virtual.png";
+
+    private static final int COST = 3;
+
+    public static final String ID = "Virtual";
+
+    public Virtual() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.SKILL, CardColor.BLUE, CardRarity.RARE, CardTarget.SELF);
+        this.exhaust = true;
+        this.isEthereal = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new AddTemporaryHPAction(p, p, AbstractDungeon.player.maxHealth));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Virtual();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            this.isEthereal = false;
+            this.rawDescription = cardStrings.UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
