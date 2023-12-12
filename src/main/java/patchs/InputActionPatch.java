8479503c4629eaa49a9edd79dce1943151e46d1b
chunk2333 <chunk2333@gmail.com>
2023-12-12 21:08:56 +0800

    新增彩蛋：科乐美秘技。

--- src/main/java/patchs/InputActionPatch.java
+++ src/main/java/patchs/InputActionPatch.java
@@ -1,12 +1,16 @@
 package patchs;
 
 import YibaMod.YibaMod;
+import actions.PlayRandomCardAction;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 @SpirePatch(clz = InputHelper.class, method = "initialize")
 public class InputActionPatch extends InputAdapter{
@@ -32,6 +36,35 @@ public class InputActionPatch extends InputAdapter{
         if (keys.contains(key1)){
             YibaMod.logger.info("触发了秘技");
             keys = "";
+            AbstractRoom currentRoom;
+            try{
+                currentRoom = AbstractDungeon.getCurrRoom();
+            } catch (NullPointerException e){
+                return true;
+            }
+
+            if (currentRoom == null){
+                return true;
+            }
+
+            if (AbstractDungeon.actionManager.turnHasEnded &&
+                    (AbstractDungeon.getCurrRoom()).phase == AbstractRoom.RoomPhase.COMBAT){
+                return true;
+            }
+
+            if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT){
+                AbstractDungeon.actionManager.addToBottom(new AbstractGameAction() {
+                    public void update() {
+                        addToBot(new PlayRandomCardAction(
+
+                                (AbstractDungeon.getCurrRoom()).monsters.getRandomMonster(null, true, AbstractDungeon.cardRandomRng), false));
+                        this.isDone = true;
+                    }
+                });
+            }
+
+            return true;
+
         }
         return true;
     }
