77938a944b90d9082f2b42d4d5c8cdd7a1726b67
chunk2333 <chunk2333@gmail.com>
2023-12-13 15:57:00 +0800

    修复了一个bug，该bug导致特殊情况下鼠标滚轮无法使用的问题。

--- src/main/java/patchs/InputActionPatch.java
+++ src/main/java/patchs/InputActionPatch.java
@@ -5,11 +5,12 @@ import actions.PlayRandomCardAction;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.badlogic.gdx.InputMultiplexer;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.helpers.input.ScrollInputProcessor;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 @SpirePatch(clz = InputHelper.class, method = "initialize")
@@ -17,10 +18,14 @@ public class InputActionPatch extends InputAdapter{
 
     public static String keys = "";
 
-    @SpirePostfixPatch
-    public static void PostFix(){
+    @SpireInsertPatch(loc = 38, localvars = {"processor"})
+    public static void PreFix(ScrollInputProcessor ___processor){
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(___processor);
+        multiplexer.addProcessor(new InputActionPatch());
         YibaMod.logger.info("监听输入事件...");
-        Gdx.input.setInputProcessor(new InputActionPatch());
+        Gdx.input.setInputProcessor(multiplexer);
+        //Gdx.input.setInputProcessor(new InputActionPatch());
     }
 
     @Override
@@ -69,4 +74,53 @@ public class InputActionPatch extends InputAdapter{
         return true;
     }
 
+    @Override
+    public boolean keyUp(int keycode) {
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return true;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return true;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return true;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return true;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return true;
+    }
+
 }
+//
+//@SpirePatch2(clz = InputHelper.class, method = "regainInputFocus")
+//class InputAcitonRegainInputFocus{
+//
+//    @SpireInsertPatch(loc = 46, localvars = {"processor", "ignoreOneCycle"})
+//    public static void fix(ScrollInputProcessor ___processor, @ByRef boolean[] ___ignoreOneCycle){
+//        InputMultiplexer multiplexer = new InputMultiplexer();
+//        multiplexer.addProcessor(___processor);
+//        multiplexer.addProcessor(new InputActionPatch());
+//        Gdx.input.setInputProcessor(multiplexer);
+//        ___ignoreOneCycle[0] = true;
+//        SpireReturn.Return();
+//    }
+//}
