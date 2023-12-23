8d5cf43de9f0c852ec7b1bc1d72f25c47e3739da
chunk2333 <chunk2333@gmail.com>
2023-12-23 18:23:49 +0800

    更新卡牌：哈哈，刀！TM全是刀！ 更好的添加小刀到手牌中。 更新卡牌：批脸。抽到时将你的手牌消耗为一张。

--- src/main/java/cards/green/Hahaknife.java
+++ src/main/java/cards/green/Hahaknife.java
@@ -1,9 +1,8 @@
-
-//Hahaknife
+//哈哈，刀！TM全是刀！
 package cards.green;
 
+import basemod.BaseMod;
 import basemod.abstracts.CustomCard;
-import YibaMod.YibaMod;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -13,32 +12,27 @@ import com.megacrit.cardcrawl.cards.tempCards.Shiv;
 import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 
 public class Hahaknife extends CustomCard{
-    //从.json文件中提取键名为LeiPu的信息
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings("Hahaknife");
-    public static final String NAME = cardStrings.NAME;
-    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "img/cards/Hahaknife.png";
-    private static final int COST = 2;
-    public static final String ID = "Hahaknife";
-    //取升级后的词条描述:
-    //public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
-    //调用父类的构造方法，传参为super(卡牌ID,卡牌名称，能量花费，卡牌描述，卡牌类型，卡牌颜色，卡牌稀有度，卡牌目标)
+    public static final String NAME = cardStrings.NAME;
+
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    public static final String IMG_PATH = "img/cards/Hahaknife.png";
+
+    private static final int COST = 2;
+
+    public static final String ID = "Hahaknife";
     public Hahaknife() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION, CardType.SKILL, CardColor.GREEN, CardRarity.UNCOMMON, CardTarget.SELF);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        //使用卡牌时触发的动作
-        //CardModifierManager.addModifier(this, new ExampleModifier());
         int cardnum_hand;
         int cardnum_add;
-        //取剩余手牌（上限手牌-现有手牌）
-        //cardnum_hand = AbstractDungeon.player.hand.size();
         cardnum_hand = p.hand.size();
-        cardnum_add = 10 - cardnum_hand;
-        YibaMod.logger.info("添加剩余手牌数量的小刀："+String.valueOf(cardnum_add));
+        cardnum_add = BaseMod.MAX_HAND_SIZE - cardnum_hand;
         //取小刀卡的对象
         AbstractCard s = (new Shiv()).makeCopy();
         //添加指定手牌数量的小刀卡           //添加剩余手牌数的小刀到手牌
@@ -48,14 +42,11 @@ public class Hahaknife extends CustomCard{
     @Override
 
     public AbstractCard makeCopy() {
-        //复制卡牌时触发
         return new Hahaknife();
     }
     @Override
     public void upgrade() {
-        //卡牌升级后的效果
         if (!this.upgraded) {
-            //更改名字和减1费
             upgradeName();
             upgradeBaseCost(1);
         }
