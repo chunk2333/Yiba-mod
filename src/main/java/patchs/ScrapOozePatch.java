285c8127825e249d6f1fe8d369d7ab2cb12fb15c
chunk2333 <chunk2333@gmail.com>
2024-02-10 12:40:08 +0800

    修复事件：破烂软泥。会多次给你塞酸液的bug。

--- src/main/java/patchs/ScrapOozePatch.java
+++ src/main/java/patchs/ScrapOozePatch.java
@@ -1,10 +1,7 @@
 package patchs;
 
 import cards.status.AcidLiquor;
-import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatches;
-import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -23,15 +20,38 @@ public class ScrapOozePatch {
             EventStrings eventStrings = CardCrawlGame.languagePack.getEventString("Scrap Ooze");
             String[] OPTIONS = eventStrings.OPTIONS;
             __instance.imageEventText.setDialogOption(OPTIONS[3] + " #r但是你手上沾着肮脏的东西!!!", new AcidLiquor());
+            buttonEffectFix.isTake = false;
             return SpireReturn.Return();
         }
     }
-    @SpirePatch(clz = ScrapOoze.class, method = "buttonEffect")
+    @SpirePatch2(clz = ScrapOoze.class, method = "buttonEffect")
     public static class buttonEffectFix{
+
+        public static boolean isTake;
+        @SpireInsertPatch(loc = 56)
+        public static void InsertFix02(ScrapOoze __instance){
+            isTake = true;
+        }
+
+        @SpireInsertPatch(loc = 87, localvars = {"screenNum"})
+        public static SpireReturn<Void> InsertFix03(ScrapOoze __instance, @ByRef int[] ___screenNum){
+            isTake = false;
+            EventStrings eventStrings = CardCrawlGame.languagePack.getEventString("Scrap Ooze");
+            String[] OPTIONS = eventStrings.OPTIONS;
+            __instance.imageEventText.updateDialogOption(0, OPTIONS[3] + " #r但是你手上沾着肮脏的东西!!!", new AcidLiquor());
+            __instance.imageEventText.removeDialogOption(1);
+            ___screenNum[0] = 1;
+            return SpireReturn.Return();
+        }
+
         @SpireInsertPatch(loc = 97)
-        public static void InsertFix(ScrapOoze __instance){
-            CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.MED, ScreenShake.ShakeDur.MED, false);
-            AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(new AcidLiquor(), Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+        public static void InsertFix01(ScrapOoze __instance){
+            if(!isTake){
+                isTake = true;
+                CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.MED, ScreenShake.ShakeDur.MED, false);
+                AbstractDungeon.effectList.add(new ShowCardAndObtainEffect(new AcidLiquor(), Settings.WIDTH / 2.0F, Settings.HEIGHT / 2.0F));
+            }
+
         }
     }
 }
